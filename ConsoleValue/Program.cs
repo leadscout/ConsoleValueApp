070d4a2921f5b07cd59e1608aff6f0b87a75ebd9
Matevz Klinc <matevz.klinc101@gmail.com>
2022-01-09 23:34:00 +0100

    Value checker application

---
+++ ConsoleValue/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleValueApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the input value: ");
+            float i = 0;
+            string Value = Console.ReadLine();
+            bool result = float.TryParse(Value, out i);
+            Console.WriteLine(result);
+        }
+    }
+}
